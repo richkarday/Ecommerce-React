c16f20905bbc4b64407d40b1b471602d324e436b
RicardoEnrique Gomez LLata Richkarday <ricardorichkarday@gmail.com>
2021-11-08 12:11:50 -0600

    Mi primer commit

---
+++ src/data.js
@@ -0,0 +1,84 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/ZcwLF3x/2.png",
+        tittle: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/PGkYWhM/4.png",
+        tittle: "WINTER SALE",
+        desc: "DON'T COMPROMISE ON STYLE",
+        bg: "fcf1ed"
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/XzNrTZb/3.png",
+        tittle: "AUTUMN SALE",
+        desc: "DON'T COMPROMISE ON STYLE",
+        bg: "fbf0f4"
+    }
+]
+
+export const categories = [
+    {
+        id: 1,
+        tittle: "SHIRT STYLE",
+        img: "https://i.ibb.co/JcTwrdp/4.jpg"
+    },
+    {
+        id: 2,
+        tittle: "LONGEWEAR LOVER",
+        img: "https://i.ibb.co/2cVJmYK/6.jpg"
+    },
+    {
+        id: 3,
+        tittle: "LIGHT JACKETS",
+        img: "https://i.ibb.co/p08R8mf/5.jpg"
+    }
+]
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/Wf5NfyT/P1.png"
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/LNRGgj4/P2.png"
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/3FxxPks/P3.png"
+    },
+    {
+        id: 4,
+        img: "https://i.ibb.co/SytSK1W/P4.png"
+    },
+    {
+        id: 5,
+        img: "https://i.ibb.co/yd9XZQY/P5.png"
+    },
+    {
+        id: 6,
+        img: "https://i.ibb.co/RBVCxVJ/P6.png"
+    },
+    {
+        id: 7,
+        img: "https://i.ibb.co/t8s3j9z/P7.png"
+    },
+    {
+        id: 8,
+        img: "https://i.ibb.co/t8s3j9z/P7.png"
+    }
+]
+
+
+
+
+
+
+
+
